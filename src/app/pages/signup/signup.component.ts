fdac8e9b47395046e2098f289dce61b4ed67ac26
Shammi2k <shammi2k@gmail.com>
2024-07-24 16:48:48 +0530

    Added required to fields and sweetAlert on user registration successful

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -6,6 +6,9 @@ import { FormsModule } from '@angular/forms';
 import { User } from '../../models/user.model';
 import { UserService } from '../../services/user.service';
 import { Observer } from 'rxjs';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material/snack-bar';
+import Swal from 'sweetalert2';
+
 
 @Component({
   selector: 'app-signup',
@@ -22,24 +25,27 @@ import { Observer } from 'rxjs';
 export class SignupComponent {
   public user = new User();
 
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private _snackBar: MatSnackBar) { }
 
   /**
    * onSubmit
    */
   public onSubmit() {
+    const config: MatSnackBarConfig = {
+      duration: 3000
+    }
     if (this.user.username == null || this.user.username == '') {
-      alert("Username must not be empty");
+      this._snackBar.open('Username cannot be empty', 'Close', config);
       return;
     }
     const userObserver: Observer<any> = {
-      next: response => {
-        alert("Success!");
+      next: (response: User) => {
+        Swal.fire("Success", `User <b>${response.username}</b> registered successfully!`, 'success');
         console.log('User registered successfully', response);
         // Handle successful registration (e.g., navigate to another page or show a success message)
       },
       error: error => {
-        alert("Error registering user");
+        this._snackBar.open('Error registering user', 'Close', config);
         console.error('Error registering user', error);
         // Handle registration error
       },
